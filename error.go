45470f9fa723b4c6203953517101d657436d5cdc
Ashley <dresswithpockets@pm.me>
2025-07-18 19:04:08 -0700

    dont mind me

--- error.go
+++ error.go
@@ -93,7 +93,7 @@ func (err Error) Error() string {
 	return str
 }
 
-func (err Error) Is(target interface{}) bool {
+func (err Error) Is(target error) bool {
 	switch v := target.(type) {
 	case ErrNo:
 		return err.Code == v
